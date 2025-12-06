7671bfffbfb8b29079c614759e614295c57dbbe7
MertalpTasdelen <tasdelenmertalp@gmail.com>
2025-12-06 18:25:56 +0300

    Add cron trigger.

---
+++ inventory_manager/check_stock_cron.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Stok kontrol scripti - Her gün sabah 9 ve öğleden sonra 3'te çalışır (Pazar hariç)
+# Kullanım: ./check_stock_cron.sh
+
+# Script dizinine git
+cd /root/yeninesilevim/inventory_manager
+
+# Virtual environment'ı aktif et
+source /root/yeninesilevim/env/bin/activate
+
+# Django management komutunu çalıştır
+python manage.py check_low_stock
+
+# Log dosyasına kaydet (proje içinde)
+echo "Stock check completed at $(date)" >> /root/yeninesilevim/inventory_manager/stock_check.log
